04f4cd5439dc7fff9647c1f0eed539fabae511a0
Billy Bunn <williammbunn@gmail.com>
2019-01-10 15:41:11 -0800

    add app.js and move JS from about_me.html to app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// window.alert("Just a little bio about Caroline Anne");
+
+// var user = prompt('So friend, What is your name?');
+// console.log('what is your name', user);
+
+
+// var answer2 = prompt (user + ", Do you want to see my photography? Answer Y or N.").toUpperCase();
+// console.log ('Do you want to see my photography?', answer2);
+
+// if (answer2 === "Y") {
+//     alert('Great!');
+// } else {
+// alert("ok,See ya!");
+// }
+
+// var years = prompt('About how many bands did I photograph this year?');
+// console.log ('About how many bands did I photograph this year?');
+
+// if (years === '40'){
+// alert(`Correct, I have photographed over 40 bands`); // over 40 bands!
+// } else 
+// {
+//     alert ('Incorrect, over 40 bands');
+// }
+
+// window.alert("Always take a picture!");
